25f4eacfbc6e2655720ab7303e5c555716db0603
Jan Głębocki <janek@e-przemek.pl>
2024-08-02 13:05:05 +0200

    prooooo

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Website Loaded');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const headingElement = document.getElementById('about-me-heading');
+    const text = 'About Me';
+    let index = 0;
+
+    function type() {
+        if (index < text.length) {
+            headingElement.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, 100); // Czas pomiędzy literami, dostosuj według potrzeb
+        }
+    }
+
+    type();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const headingElement = document.getElementById('main-name');
+    const text = 'Jan Głębocki';
+    let index = 0;
+
+    function type() {
+        if (index < text.length) {
+            headingElement.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, 100); // Czas pomiędzy literami, dostosuj według potrzeb
+        }
+    }
+
+    type();
+});
